074e751e4e6ff10526e38f64ad2472327972eb8d
cebasypia.dev01 <cebasypia.tskm8+dev01@gmail.com>
2022-08-14 18:18:27 +0900

    Configure ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
 
-function App(): JSX.Element {
+const App = (): JSX.Element => {
   const [count, setCount] = useState(0);
 
   return (
@@ -25,6 +25,6 @@ function App(): JSX.Element {
       <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </div>
   );
-}
+};
 
 export default App;
